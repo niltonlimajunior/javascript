fd87298689307faff91c977ff1341cc66eec9997
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-07 16:44:15 -0300

    att

---
+++ aula11/ex.008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+    if (vel > 60){ //Condição simples! quando não tem o else
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto`)
