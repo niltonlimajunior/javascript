6fbc14da1cbb2643dcb520a40fa107091f731b95
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-07 19:40:22 -0300

    att

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+var agora = new Date ()
+var hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12 && hora > 4) {
+    console.log(`Bom dia!`)
+} else if (hora > 11 && hora < 19) {
+    console.log('Boa tarde!') 
+} else {
+    console.log('Boa noite!')
+}  
