d13d5ee6165152843d6cbbba4cc34784c58eb6d8
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-10 16:57:07 -0300

    att

--- aula12ex/ex014/script/script.js
+++ aula12ex/ex014/script/script.js
@@ -2,18 +2,19 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 8
-
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >=o && hora <12) {
+    if (hora >= 0 && hora <12) {
         //BOM DIA!
         img.src = 'manha.png'
-    } else if (hora >=12 && hora <18) {
+        document.body.style.background = '#F3DEC5'
+    } else if (hora >=12 && hora <=18) {
         // BOA TARDE!
         img.src = 'tarde.png'
+        document.body.style.background = '#BBD4AC'
     } else {
         //BOA NOITE
         img.src = 'noite.png'
+        document.body.style.background = '#0F4FA6'
     }
 }
