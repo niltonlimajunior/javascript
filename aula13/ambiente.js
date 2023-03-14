fcb539b8c36bc0f74b828d519d48b200a1df1730
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-14 18:54:05 -0300

    att

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+/*
+var contador = 1
+while (contador <= 10){
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+}
+*/
+
+
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+} while (contador <= 10)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
