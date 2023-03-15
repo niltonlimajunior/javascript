6cd219ccdfa6e5422dc699d8a40e84e4a4871b8d
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-15 15:41:17 -0300

    Update script.js

--- aula14ex/ex016/script/script.js
+++ aula14ex/ex016/script/script.js
@@ -5,15 +5,29 @@ function contar() {
     let res = document.getElementById('ires')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-       res.innerHTML = 'Contando: '
-       let i = Number(inicio.value)
-       let f = Number(fim.value)
-       let p = Number(passo.value)
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo Inválido! Considerando passo 1')
+            p = 1 
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let contador = i; contador <= f; contador += p) {
+                res.innerHTML += `${contador} \u{1F449}`
+        }
+       } else {
+            //contagem regressiva
+            for(contador = i;contador >= f;contador -= p) {
+                res.innerHTML += `${contador} \u{1F449}`
+            }
 
-       for(let contador = i; contador <= f; contador += p) {
-            res.innerHTML += `${contador} `
        }
+       res.innerHTML += `\u{1F3C1}`
     }
  }
