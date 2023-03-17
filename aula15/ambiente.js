f2b439330023a0498522d7fb763b9b4d8170a0ed
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-17 19:30:12 -0300

    att

---
+++ aula15/ambiente.js
@@ -0,0 +1,20 @@
+let num = [5,8,2,9,3]
+num.push(6)
+num.sort()
+
+
+
+//num[5] = 7
+//num.push(8) //adicionar valores na última posição do vetor
+//num.length //comprimento do array
+//num.sort() // coloca todos os elementos em ordem crescente
+
+
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O segundo valor do vetor é ${num[1]}`)
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+
